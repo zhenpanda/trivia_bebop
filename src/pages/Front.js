b6692ed982d8a00d36639513a829db4811662ab8
zhenting zhou <zhentzhou@gmail.com>
2016-11-04 11:17:47 -0400

    sure?

--- src/pages/Front.js
+++ src/pages/Front.js
@@ -10,10 +10,10 @@ export default class Front extends React.Component {
       <div className="App">
         <div className="container">
           <img id="top-space" src={landing} alt={"logo"}/>
-        </div>
-        <div id="start-area">
-          {this.props.children}
-          <Link to="game" className="waves-effect waves-light btn" id="start-btn">Start Game</Link>
+          <div id="start-area">
+            {this.props.children}
+            <Link to="game" className="waves-effect waves-light btn" id="start-btn">Start Game</Link>
+          </div>
         </div>
       </div>
     );
